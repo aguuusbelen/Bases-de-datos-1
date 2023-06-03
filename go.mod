fef1859a4b1823545e9154e441d3382f8fef6676
Agustina Mendez <lilo@hawaii.unix.ar>
2023-06-02 21:45:50 -0300

    prueba go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module buffagna-curcio-mendez-tp
+
+go 1.19
+
+require github.com/lib/pq v1.10.9
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "database/sql"
+    "fmt"
+    _ "github.com/lib/pq"
+    "log"
+)   
+
+func main() {
+    fmt.Println(time.Now())
+}
+

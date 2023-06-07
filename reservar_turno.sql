1e87920ed174659a2226f529dafba64d7647e5f5
Agustina Mendez <lilo@hawaii.unix.ar>
2023-06-07 12:09:49 -0300

    reservar turno

---
+++ reservar_turno.sql
@@ -0,0 +1,28 @@
+create function Reservar_turno(nro_paciente int, dni_medique int, fecha timestamp) returns boolean as $$
+declare
+	
+begin
+	select * from paciente where nro_paciente = paciente.nro_paciente;
+	
+	if not found then
+		raise 'el número de paciente no se encuentra registrado' , nro_paciente;
+	end if;
+	
+	select * from medique where dni_medique = medique.dni_medique;
+	
+	if not found then
+		raise 'no se encontró medique' , dni_medique;
+	end if;
+	--falta verificar obra social
+	--falta verificar turnos pendientes de pacientes
+	select * from turno where fecha = turno.fecha and dni_medique = dni_medique and estado = "disponible";
+	
+	if not found then
+		raise 'no hay disponibilidad de turnos para la fecha requerida' , fecha;
+	else
+		update turno set nro_paciente = nro_paciente, nro_obra_social_consulta = paciente.nro_obra_social,
+		nro_afiliade_consulta = paciente.nro_afiliade, monto_paciente = cobertura.monto_paciente,
+		monto_obra_social = cobertura.monto_obra_social, f_reserva = time.Now(), estado = "reservado" where (fecha = turno.fecha and dni_medique = dni_medique and estado = "disponible");
+	end if;
+
+	

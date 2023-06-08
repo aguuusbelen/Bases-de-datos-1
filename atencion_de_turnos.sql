feff2504a941ce27485b2f1a884e88af5d557fd4
Matias Curcio <juanmatiascurcio@gmail.com>
2023-06-08 20:12:20 +0000

    corregimos error

--- atencion_de_turnos.sql
+++ atencion_de_turnos.sql
@@ -1,21 +1,22 @@
-create or replace function Atencion_de_turno(nro_turno int) returns boolean as $$
+create or replace function Atencion_de_turno(turno_nro int) returns boolean as $$
 declare
 	t turno%rowtype;
 begin
-	t := (select * from turno where nro_turno = turno.nro_turno);
+	select * into t from turno where turno_nro = turno.nro_turno;
 	
-		if not t then
-			raise 'número de turno no válido', nro_turno; 
+		if not found then
+			raise 'número de turno no válido'; 
 		else 
 			if  not (t.estado = 'reservado') then
 				raise 'turno no reservado'; 
 			else
-				if not (t.fecha = localtimestamp) then
-					raise 'turno no corresponde a la fecha del dia', t.fecha;
+				if not (t.fecha::date = current_date) then
+					raise 'turno no corresponde a la fecha del dia';
 					return false;
 				else
-					update turno set estado = 'atendido' where turno.nro_turno = nro_turno;
+					update turno set estado = 'atendido' where turno.nro_turno = turno_nro;
 					return true;
+				end if;
 			end if;
 		end if;
 		

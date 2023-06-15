6af5e9e960e45f16177fda802ca49c3bbf7ccd50
aguuus_belen <agustinabmendez@gmail.com>
2023-06-15 14:27:33 -0300

    informe

--- archivosSQL/atencion_de_turnos.sql
+++ archivosSQL/atencion_de_turnos.sql
@@ -9,19 +9,18 @@ begin
 		if not found then
 			insert into error (f_turno, nro_consultorio, dni_medique, nro_paciente, operacion, f_error, motivo) 
 								values (t.fecha, t.nro_consultorio, t.dni_medique, t.nro_paciente, 'atención', fecha_actual, 'número de turno no válido');
-			raise 'número de turno no válido'; 
 			return false;
 		else 
 			if  not (t.estado = 'reservado') then
 				insert into error (f_turno, nro_consultorio, dni_medique, nro_paciente, operacion, f_error, motivo) 
 								values (t.fecha, t.nro_consultorio, t.dni_medique, t.nro_paciente, 'atención', fecha_actual, 'turno no reservado');
-				raise 'turno no reservado'; 
+				 
 				return false;
 			else
 				if not (t.fecha::date = current_date) then
 					insert into error (f_turno, nro_consultorio, dni_medique, nro_paciente, operacion, f_error, motivo) 
 								values (t.fecha, t.nro_consultorio, t.dni_medique, t.nro_paciente, 'atención', fecha_actual, 'turno no corresponde a la fecha del día');
-					raise 'turno no corresponde a la fecha del dia';
+					
 					return false;
 				else
 					update turno set estado = 'atendido' where turno.nro_turno = turno_nro;
